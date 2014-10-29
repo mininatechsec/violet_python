b8ea5bbdf31e8c0abde9c7fdedbda5992d45723c
Andrew Sidorov <qqw1mb0@gmail.com>
2014-10-29 16:45:04 +0400

    sshBrute.py init

---
+++ ssh/sshBrute.py
@@ -0,0 +1,25 @@
+import pexpect
+PROMPT = ['# ', '>>> ', "> ", "\$ "]
+
+def send_command(child, cmd):
+	child.sendline(cmd)
+	child.expect(PROMPT)
+	print child.before
+
+def connect(user, host, password):
+	ssh_newkey = 'Are you sure you want to continue connecting'
+	connStr = 'ssh ' + user + '@' + host
+	child = pexpect.spawn(connStr)
+	ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])
+	if ret == 0:
+		print '[-] Error Connecting'
+		return
+	if ret == 1:
+		child.sendline('yes')
+	ret = child.expect([pexpect.TIMEOUT, '[P|p]assword:'])
+	if ret == 0:
+		print '[-] Error Connecting'
+		return
+	child.sendline(password)
+	child.expect(PROMPT)
+	return child
