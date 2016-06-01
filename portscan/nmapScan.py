c3061ee3ee4e975dcea99f3be4c7ebf76baaf7f5
mininatechsec <jcamzezr@gmail.com>
2016-05-31 19:31:17 -0500

    Add files via upload

--- portscan/nmapScan.py
+++ portscan/nmapScan.py
@@ -5,7 +5,7 @@ def nmapScan(tgtHost, tgtPort):
 	nmScan = nmap.PortScanner()
 	nmScan.scan(tgtHost, tgtPort)
 	state = nmScan[tgtHost]['tcp'][int(tgtPort)]['state']
-	print " [*] " + tgtHost + " tcp/" + tgtPort + " " + state
+	print ("[*]" + tgtHost + "tcp/"+tgtPort +" "+state)
 
 def main():
 	parser = optparse.OptionParser('usage %prog -H <target host> -p <target port>')
@@ -15,7 +15,7 @@ def main():
 	tgtHost = options.tgtHost
 	tgtPorts = str(options.tgtPort).split(',')
 	if (tgtHost == None) | (tgtPorts[0] == None):
-		print parser.usage
+		print (parser.usage)
 		exit(0)
 	for tgtPort in tgtPorts:
 		nmapScan(tgtHost, tgtPort)
